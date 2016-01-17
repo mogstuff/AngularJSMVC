45d360e347d6f4ea971f5c2a80115f4459e620d2
mogstuff <markmorgan.mcp@gmail.com>
2016-01-17 18:20:47 +0000

    added vs project files

---
+++ Models/BookDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSMVC.Models
+{
+    public class BookDBContext : DbContext
+    {
+        public BookDBContext()
+            : base("name=BookDBContext") 
+        { 
+        
+        }
+        
+        
+        public DbSet<Book> book { get; set; }
+
+    }
+}
